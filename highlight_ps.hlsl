ecd1191efae37a36e006fd2de1b375a00b271eec
Gøran <goran@ekerbakke.no>
2018-03-18 00:06:58 +0000

    Implemented untested highlight shaders

--- highlight_ps.hlsl
+++ highlight_ps.hlsl
@@ -1,3 +1,8 @@
+cbuffer HighlightProperties : register(b0)
+{
+    float4 color;
+};
+
 float4 main(float4 position : SV_Position) : SV_Target
 {
     return float4(1.f, 0.f, 0.f, 1.f);
--- highlight_vs.hlsl
+++ highlight_vs.hlsl
@@ -1,4 +1,35 @@
-float4 main(float4 position : POSITION) : SV_Position
+cbuffer Matrices : register(b0)
 {
-    return float4(0.f, 0.f, 0.f, 1.f);
+    // row_major?
+    matrix world;
+    matrix view;
+    matrix projection;
+    matrix worldView;   // Improperly configured (DO NOT USE!)
+};
+
+struct VSInput
+{
+    float3 position : SV_Position;
+    float3 normal : NORMAL;
+    float4 tangent : TANGENT;   // Ignored
+    float4 color : COLOR;
+    float2 texCoord : TEXCOORD; // Ignored
+};
+
+struct VSOutput
+{
+    float4 position : SV_POSITION;
+};
+
+float4 main(VSInput input) : SV_Position
+{
+    static const float DIST = 2.f;
+
+    float4 newPosition = float4(input.position + (input.normal * DIST), 1.f);
+
+    newPosition = mul(newPosition, world);
+    newPosition = mul(newPosition, view);
+    newPosition = mul(newPosition, projection);
+
+    return newPosition;
 }
